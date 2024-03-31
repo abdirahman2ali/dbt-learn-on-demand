27b5774b27d73891d6f1f1ad7823193d1299f6be
Abdirahman Ali <abdirahman2ali@gmail.com>
2024-03-31 06:19:39 +0000

    Add a model that pivoted payment methods using Jinja

---
+++ models/marts/core/core.md
@@ -0,0 +1,5 @@
+{% docs fct_customer_orders %}
+
+    One row per order. This view joins customers table to identify customers that purchased and brings in aggregated metrics from int_orders.
+
+{% enddocs %}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -16,6 +16,7 @@ transformed as (
 
         id as payment_id,
         orderid as order_id,
+        paymentmethod as payment_method,
         status as payment_status,
         round(amount/100.0,2) as payment_amount,
 
